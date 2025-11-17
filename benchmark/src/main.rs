771ed507fd9b8848ec4cfd16d5c952dcfb2435e8
Alexander Binaei <bigtimetapin@gmail.com>
2025-11-17 15:07:46 +0400

    env log levels

--- benchmark/src/main.rs
+++ benchmark/src/main.rs
@@ -41,10 +41,7 @@ fn estimate_epoch_from_time(date_str: &str) -> Result<i64, Box<dyn Error>> {
 async fn main() -> Result<(), Box<dyn Error>> {
     // Initialize tracing subscriber to enable logging at INFO level
     tracing_subscriber::fmt()
-        .with_env_filter(
-            tracing_subscriber::EnvFilter::from_default_env()
-                .add_directive(tracing::Level::INFO.into()),
-        )
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .init();
 
     let db_url = env::var("DB_CONNECTION_URL")
